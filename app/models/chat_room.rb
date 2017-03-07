70629d64138cb0cd9cc189d37b4ffc44a531ebbc
Yohei Senju <yohei.senju@gmail.com>
2017-03-08 03:15:43 +0900

    Mount enum

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -2,4 +2,17 @@ class ChatRoom < ApplicationRecord
   has_many :chat_room_users
   has_many :users, through: :chat_room_users
   has_many :messages
+
+  STATUS_HASH  = {
+    personal: 0,
+    group: 1
+  }
+
+  enum status: ChatRoom::STATUS_HASH
+
+  def update_group!
+    if self.users.count > 2
+      self.update(status: 2)
+    end
+  end
 end
