481e1b3987f9911e9573ae8994eb333d1e99d6ac
Yohei Senju <yohei.senju@gmail.com>
2017-03-08 02:37:00 +0900

    Add base model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,27 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   mount_uploader :image, UserImageUploader
+
+
+  has_many :friend_relations
+  has_many :friends, through: :friend_relations, class_name: "User"
+
+  has_many :messages
+  has_many :chat_room_users
+  has_many :chat_rooms, through: :chat_room_users
+
+
+  def add_friend(user)
+    self.friend_relations.create(friend_id: user.id)
+    user.friend_relations.create(friend_id: self.id)
+  end
+
+  def quit_friend(user_id)
+    self
+      .friend_relations
+      .where(friend_id: user_id)
+      .first
+      .destroy
+  end
+
 end
