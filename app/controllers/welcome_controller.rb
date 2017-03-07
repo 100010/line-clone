f788aa0c5ea47ec4de30917a329db75f4d1fa086
Yohei Senju <yohei.senju@gmail.com>
2017-03-07 22:21:09 +0900

    Authenticate when user is logging in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,15 @@
 class WelcomeController < ApplicationController
 
+  before_action :autheticated_user
+
   layout 'empty'
 
   def index
+  end
+
+  private def autheticated_user
+    if current_user
+      redirect_to home_user_path(current_user)
+    end
   end
 end
