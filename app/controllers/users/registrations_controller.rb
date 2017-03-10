30ea75f0a1bd4c3fb87369eba4bf330d0f67b622
Yohei Senju <yohei.senju@gmail.com>
2017-03-11 05:52:33 +0900

    Make user updatable

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
- before_action :configure_sign_up_params, only: [:create]
-# before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
-  layout 'main'
+  layout 'main', only: [:new]
 
   # GET /resource/sign_up
   # def new
@@ -51,9 +51,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.
+      permit(:account_update,
+       keys: [
+          :name,
+          :image
+        ]
+      )
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
